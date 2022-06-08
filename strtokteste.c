3fa7e2567258c125c9aa39a265dab78b38f06cb9
Gabe <55238431+GabePeres@users.noreply.github.com>
2022-06-08 12:11:36 -0300

    Update strtokteste.c

--- strtokteste.c
+++ strtokteste.c
@@ -1,13 +1,15 @@
 /* strtok example */
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main ()
 {
   char str[] ="{85,Wolverine,m,135,88,1.63,(99)99999-9999}";
   char * pch;
-  printf ("Splitting string \"%s\" into tokens:\n",str);
+  //printf ("Splitting string \"%s\" into tokens:\n",str);
   pch = strtok (str,",{}");
+  printf ("fora 2: %d\n",atoi(pch));
   while (pch != NULL)
   {
     printf ("%s\n",pch);
